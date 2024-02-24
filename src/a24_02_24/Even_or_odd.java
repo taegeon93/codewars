c907f1d76b7656de9cc3cd3306f9357465482136
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-02-24 23:40:04 +0900

    Add files via upload
    24!

---
+++ src/a24_02_24/Even_or_odd.java
@@ -0,0 +1,15 @@
+package a24_02_24;
+
+public class Even_or_odd {
+	
+	public Even_or_odd() {
+		int x = 5;
+		
+		 if(x%2==0){
+	         System.out.println("Even");
+	        }else{
+	        	 System.out.println("Odd");
+	        }
+	    
+	}
+}
