6c0ccf345091eb75db9d98f79dbed3ce3000a013
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-02-25 22:23:46 +0900

    Add files via upload

---
+++ src/a24_02_25/Convert_a_Number_to_a_String.java
@@ -0,0 +1,10 @@
+package a24_02_25;
+
+public class Convert_a_Number_to_a_String {
+	
+	public Convert_a_Number_to_a_String() {
+		int num = 20;
+		int num2 = 21;
+		System.out.println(String.valueOf(num)+String.valueOf(num2));
+	}
+}
