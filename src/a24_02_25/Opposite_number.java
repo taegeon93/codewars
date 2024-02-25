1d9e1d06172832a110a00f7ac1be08a589cfbdaf
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-02-25 22:52:40 +0900

    Add files via upload

---
+++ src/a24_02_25/Opposite_number.java
@@ -0,0 +1,12 @@
+package a24_02_25;
+
+public class Opposite_number {
+    public Opposite_number() {
+		int number = 5;
+		int a = (number > 0) ? number*-1 : number*1;
+		
+		System.out.println(a);
+    }
+	
+
+}
