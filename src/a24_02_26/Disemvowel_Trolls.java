0ad0a928872e0bdbd67a41f386469fb4f41a43fa
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-02-26 19:19:58 +0900

    Add files via upload

---
+++ src/a24_02_26/Disemvowel_Trolls.java
@@ -0,0 +1,13 @@
+package a24_02_26;
+
+public class Disemvowel_Trolls {
+	public Disemvowel_Trolls() {
+		String b ="This website is for losers LOL";
+		String a ="(?i)[aeiou]";
+		
+		
+		
+		System.out.println(b.replaceAll(a,""));
+		
+	}
+}
