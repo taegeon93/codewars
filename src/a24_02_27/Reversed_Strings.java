972f6747f367b6415081d0d39ef082bdb6b19ec9
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-02-27 23:50:34 +0900

    Add files via upload

---
+++ src/a24_02_27/Reversed_Strings.java
@@ -0,0 +1,11 @@
+package a24_02_27;
+
+public class Reversed_Strings {
+	public Reversed_Strings() {
+		String str = "abc";
+		StringBuffer sb = new StringBuffer(str);
+	    String reverse = sb.reverse().toString();
+	    System.out.println(reverse);
+	}
+
+}
