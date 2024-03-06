fa050d4ed84593dcc488b25a829b3045a8b9f968
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-03-06 16:21:40 +0900

    Add files via upload

---
+++ src/a24_03_06/Convert_boolean_values_to_strings_Yes_or_No.java
@@ -0,0 +1,13 @@
+package a24_03_06;
+
+public class Convert_boolean_values_to_strings_Yes_or_No {
+	public Convert_boolean_values_to_strings_Yes_or_No() {
+		boolean a = true;
+		boolean b = false;
+		
+			System.out.println((b)? "Yes":"No");
+			
+		
+	}
+}
+
