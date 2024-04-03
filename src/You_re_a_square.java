3cd5607fc1eef7fcaf26b89aa2a90fbcbc495449
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-04-03 22:05:10 +0900

    Add files via upload

---
+++ src/You_re_a_square.java
@@ -0,0 +1,14 @@
+package codewars;
+
+public class You_re_a_square {
+
+	
+	public You_re_a_square() {
+		
+		int a = 36;
+		
+	 boolean b = Math.sqrt(a)%1 == 0 ? true : false; 
+	 
+	 //   삼항연산자의 불필요  return Math.sqrt(a)%1==0      만으로도 boolean 타입을 반환 가
+	}
+}
