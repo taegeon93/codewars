c2c108ab37b4055c8586ae2f923b4ccecf9b926b
taegeon93 <157105175+taegeon93@users.noreply.github.com>
2024-04-04 22:56:01 +0900

    Add files via upload

---
+++ src/Remove_First_and_Last_Character.java
@@ -0,0 +1,13 @@
+package codewars;
+
+public class Remove_First_and_Last_Character {
+
+		public Remove_First_and_Last_Character() {
+			
+			
+			String a = "abcd";
+			
+			System.out.println(a.substring(1,a.length()-1));
+			
+		}
+}
